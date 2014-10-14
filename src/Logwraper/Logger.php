d5914779acc81f482b1dfe68581c4a362c231f26
纪鹏程 <jipengcheng@gmail.com>
2014-10-14 18:14:16 +0800

    Create Logger.php

---
+++ src/Logwraper/Logger.php
@@ -0,0 +1,16 @@
+<?php
+namespace Logwrapper;
+
+class Logger {
+
+  private $log = null;
+
+  public function __construct($file){
+    $log = new Monolog\Logger('name');
+    $log->pushHandler(new Monolog\Handler\StreamHandler($file, Monolog\Logger::WARNING));
+  }
+
+  public function logwarn($msg){
+    $log->addWarning($msg);
+  }
+}
