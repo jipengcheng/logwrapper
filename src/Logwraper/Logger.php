449eb78b04977278699b3138cfaf0798b288c137
纪鹏程 <jipengcheng@gmail.com>
2014-10-14 19:00:00 +0800

    Update Logger.php

--- src/Logwraper/Logger.php
+++ src/Logwraper/Logger.php
@@ -6,11 +6,11 @@ class Logger {
   private $log = null;
 
   public function __construct($file){
-    $log = new \Monolog\Logger('name');
-    $log->pushHandler(new Monolog\Handler\StreamHandler($file, Monolog\Logger::WARNING));
+    $this->log = new \Monolog\Logger('name');
+    $this->log->pushHandler(new \Monolog\Handler\StreamHandler($file, \Monolog\Logger::WARNING));
   }
 
   public function logwarn($msg){
-    $log->addWarning($msg);
+    $this->log->addWarning($msg);
   }
 }
